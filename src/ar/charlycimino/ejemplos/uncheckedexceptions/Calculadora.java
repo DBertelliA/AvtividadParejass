42063aafed08f10ed6e56c80ee1f5a30c4b96b00
paula <paularibot15@gmail.com>
2026-04-18 10:15:39 +0200

    Comentario corregido de Calculadora.java con CheckStyle

--- src/ar/charlycimino/ejemplos/uncheckedexceptions/Calculadora.java
+++ src/ar/charlycimino/ejemplos/uncheckedexceptions/Calculadora.java
@@ -2,7 +2,6 @@
 package ar.charlycimino.ejemplos.uncheckedexceptions;
 
 /**
- *
  * @author Charly Cimino
  * Aprendé más Java en mi canal: https://www.youtube.com/c/CharlyCimino
  * Encontrá más código en mi repo de GitHub: https://github.com/CharlyCimino
